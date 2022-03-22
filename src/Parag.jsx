c9b669934ed0d36b583e1a584f6eadee8dc3a1dc
saloni23j <mathursaloni123@gmail.com>
2022-03-22 23:45:26 +0530

    Created  a paragraph component

---
+++ src/Parag.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Parag(){
+    return <p> Hello I am creating a paragraph component and secretely praying that it works</p>;
+}
+export default Parag;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Heading from './Heading'
+import Heading from './Heading';
+import Parag from './Parag';
 
 
 ReactDOM.render(
 <>
 <Heading></Heading>
+<Parag></Parag>
 <h1> doing something</h1>
 </>, document.getElementById('root')
 
