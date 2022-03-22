b5c6a1e49c1f7ef31a78c0b178118e80ecae5654
saloni23j <mathursaloni123@gmail.com>
2022-03-22 23:56:42 +0530

    created list component

---
+++ src/Lists.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Lists(){
+    return (
+        
+       <ul>
+           <li>list item 1</li>
+           <li>List item 2</li>
+           <li>List item 3 </li>
+       </ul>);
+
+  }
+
+export default Lists;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Heading from './Heading';
 import Parag from './Parag';
+import Lists from './Lists';
+
 
 
 ReactDOM.render(
 <>
 <Heading></Heading>
 <Parag></Parag>
+<Lists></Lists>
 <h1> doing something</h1>
 </>, document.getElementById('root')
 
